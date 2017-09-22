514dc4144f9c35ae7da3acd12f31c858a8d868cc
高旭 <sakura@gaoxudeMacBook-Pro.local>
2017-09-23 01:28:06 +0800

    hlselab2

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  command
+//
+//  Created by 高旭 on 2017/9/23.
+//  Copyright © 2017年 高旭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
